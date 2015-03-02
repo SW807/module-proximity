efd5d5185ae8d18e5038c346d236d8d1c0075ae9
Mathias Winde <Mapede11@student.aau.dk>
2015-03-02 08:50:08 +0100

    Benytter nu den opdateret version af module_lib

--- app/src/main/java/dk/aau/cs/psylog/psylog_proximitymodule/ProxService.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog_proximitymodule/ProxService.java
@@ -1,14 +1,10 @@
 package dk.aau.cs.psylog.psylog_proximitymodule;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
 import dk.aau.cs.psylog.module_lib.SuperService;
 
 public class ProxService extends SuperService {
     @Override
-    public void onCreate(){
-        super.onCreate();
+    public void setSensor() {
         sensor = new ProximityListener(this);
     }
 }
