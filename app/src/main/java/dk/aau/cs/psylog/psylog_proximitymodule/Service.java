da4d27fad0d9ad3936fca3712caf7cf9b33f421e
Lasse Vang Gravesen <gravesenlasse@gmail.com>
2015-03-03 09:40:04 +0100

    Omnavngiv service til Service så

--- app/src/main/java/dk/aau/cs/psylog/psylog_proximitymodule/Service.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog_proximitymodule/Service.java
@@ -2,7 +2,7 @@ package dk.aau.cs.psylog.psylog_proximitymodule;
 
 import dk.aau.cs.psylog.module_lib.SuperService;
 
-public class ProxService extends SuperService {
+public class Service extends SuperService {
     @Override
     public void setSensor() {
         sensor = new ProximityListener(this);
